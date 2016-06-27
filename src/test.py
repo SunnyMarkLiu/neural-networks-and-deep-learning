7fa1728e1ba50f5dd21672325519d8c6db8c7a01
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2016-06-27 22:50:03 +0800

    using two learning rates

--- src/test.py
+++ src/test.py
@@ -18,16 +18,31 @@ print 'training network...'
 # nn = network.Network([784, 30, 10])
 # nn.SGD(training_data, 30, 10, 2.0, validation_data)
 
-nn = network2.Network([784, 30, 10])
+nn = network2.Network([784, 100, 10])
 training_cost, training_accuracy, evaluation_cost, evaluation_accuracy \
-    = nn.SGD(training_data, 30, 10, 2.0,
-             lmbda=1,
+    = nn.SGD(training_data, 20, 10, 0.1,
+             lmbda=0.01,
              validation_data=validation_data,
              monitor_training_cost=True,
              monitor_training_accuracy=True,
              monitor_evaluation_cost=True,
              monitor_evaluation_accuracy=True
              )
+
+training_cost2, training_accuracy2, evaluation_cost2, evaluation_accuracy2 \
+    = nn.SGD(training_data, 40, 10, 0.05,
+             lmbda=0.01,
+             validation_data=validation_data,
+             monitor_training_cost=True,
+             monitor_training_accuracy=True,
+             monitor_evaluation_cost=True,
+             monitor_evaluation_accuracy=True
+             )
+
+training_cost.extend(training_cost2)
+training_accuracy.extend(training_accuracy2)
+evaluation_cost.extend(evaluation_cost2)
+evaluation_accuracy.extend(evaluation_accuracy2)
 
 import matplotlib.pyplot as plt
 
@@ -46,5 +61,5 @@ plt.title("training_cost")
 plt.subplot(224)
 plt.plot(range(0, len(training_accuracy)), training_accuracy, color="green")
 plt.title("training_accuracy")
+plt.savefig("network_evaluation3.png", dip=72)
 plt.show()
-plt.savefig("network_evaluation.png", dip=72)
